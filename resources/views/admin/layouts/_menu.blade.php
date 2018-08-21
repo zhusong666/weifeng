127417798c97dbc1c28f65af12653f897301b7cd
zou <501850236@qq.com>
2018-08-21 11:41:26 +0800

    2018年8月21日 11:41:23

--- resources/views/admin/layouts/_menu.blade.php
+++ resources/views/admin/layouts/_menu.blade.php
@@ -1,25 +1,36 @@
 <aside class="Hui-aside">
 	<div class="menu_dropdown bk_2">
-		<dl id="menu-article">
-			<dt><i class="Hui-iconfont">&#xe616;</i> 帮助管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+		<dl id="menu-member">
+			<dt><i class="Hui-iconfont">&#xe60d;</i> 用户管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
 			<dd>
 				<ul>
-					<li><a href="/admin/article/create" title="文章管理">添加帮助</a></li>
-					<li><a href="/admin/article" title="文章管理">帮助中心</a></li>
+					<li><a href="/admin/user" title="会员列表">用户列表</a></li>
+					<li><a href="/admin/user/create" title="添加会员">添加用户</a></li>
 				</ul>
 			</dd>
 		</dl>
+		<dl id="menu-admin">
+			<dt><i class="Hui-iconfont">&#xe62d;</i> 管理员管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+			<dd>
+				<ul>
+					<li><a href="/admins/role" title="角色管理">角色管理</a></li>
+					<li><a href="/admins/permission" title="权限管理">权限管理</a></li>
+				</ul>
+			</dd>
+		</dl>
+
+		
 		<dl id="menu-article">
 			<dt><i class="Hui-iconfont">&#xe616;</i> 轮播管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
 			<dd>
 				<ul>
 					<li><a href="/admin/lunbo/create" title="文章管理">添加轮播</a></li>
-					<li><a href="/admin/lunbo" title="文章管理">轮播中心</a></li>
+					<li><a href="/admin/lunbo" title="文章管理">轮播浏览</a></li>
 				</ul>
 			</dd>
 		</dl>
 		<dl id="menu-product">
-			<dt><i class="Hui-iconfont">&#xe620;</i> 产品管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+			<dt><i class="Hui-iconfont">&#xe72d;</i> 分类管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe72d;</i></dt>
 			<dd>
 				<ul>
 					
@@ -30,11 +41,19 @@
 			</dd>
 		</dl>
 		<dl id="menu-product">
-			<dt><i class="Hui-iconfont">&#xe620;</i> 商品管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+			<dt><i class="Hui-iconfont">&#xe620;</i> 商品管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe620;</i></dt>
 			<dd>
 				<ul>
 					<li><a href="/admins/goods" title="查看商品">查看商品</a></li>
 					<li><a href="/admins/goods/create" title="添加商品">添加商品</a></li>
+				</ul>
+			</dd>
+		</dl>
+		<dl id="menu-admin">
+			<dt><i class="Hui-iconfont">&#xe620;</i> 订单管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+			<dd>
+				<ul>
+					<li><a href="/admin/order" title="浏览订单">浏览订单</a></li>
 				</ul>
 			</dd>
 		</dl>
@@ -47,33 +66,16 @@
 				</ul>
 			</dd>
 		</dl>
-		<dl id="menu-member">
-			<dt><i class="Hui-iconfont">&#xe60d;</i> 用户管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
+		<dl id="menu-article">
+			<dt><i class="Hui-iconfont">&#xe616;</i> 帮助管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
 			<dd>
 				<ul>
-					<li><a href="/admin/user" title="会员列表">会员列表</a></li>
-					<li><a href="/admin/user/create" title="添加会员">添加会员</a></li>
+					<li><a href="/admin/article/create" title="文章管理">添加帮助</a></li>
+					<li><a href="/admin/article" title="文章管理">帮助浏览</a></li>
 				</ul>
 			</dd>
 		</dl>
-		<dl id="menu-admin">
-			<dt><i class="Hui-iconfont">&#xe62d;</i> 管理员管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
-			<dd>
-				<ul>
-					<li><a href="/admins/role" title="角色管理">角色管理</a></li>
-					<li><a href="/admins/permission" title="权限管理">权限管理</a></li>
-					<li><a href="admin-list.html" title="管理员列表">管理员列表</a></li>
-				</ul>
-			</dd>
-		</dl>
-		<dl id="menu-admin">
-			<dt><i class="Hui-iconfont">&#xe620;</i> 订单管理<i class="Hui-iconfont menu_dropdown-arrow">&#xe6d5;</i></dt>
-			<dd>
-				<ul>
-					<li><a href="/admin/order" title="浏览订单">浏览订单</a></li>
-				</ul>
-			</dd>
-		</dl>
+		
 	</div>
 </aside>
 <div class="dislpayArrow hidden-xs"><a class="pngfix" href="javascript:void(0);" onClick="displaynavbar(this)"></a></div>
